37e9a175ef18311fc8b07c0cf90f4b6a15545c5b
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2021-12-26 15:55:01 +0900

    feat deal with cors warning: allow_origin any

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,22 @@
-use axum::{routing::get, Router};
+use axum::{http::Method, routing::get, Router};
 use server::route::hit_yelp_api::businesses_search::businesses_search_handler;
 use std::net::SocketAddr;
+use tower_http::cors::{any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/yelp/businesses/search", get(businesses_search_handler));
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+        .route("/yelp/businesses/search", get(businesses_search_handler))
+        .layer(
+            // see https://docs.rs/tower-http/latest/tower_http/cors/index.html
+            // for more details
+            CorsLayer::new()
+                .allow_origin(any())
+                .allow_methods(vec![Method::GET]),
+        );
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8888));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
