856327732b792009258aa4718a3b82b63e5cfaf4
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2022-03-19 17:43:03 +0900

    feat clientでlimitを指定できるようにする

--- client/api_request/yelpBusinessSerachAPI.ts
+++ client/api_request/yelpBusinessSerachAPI.ts
@@ -4,24 +4,25 @@ export type conditions = {
   latitude?: string;
   longitude?: string;
   range?: string;
+  limit?: string;
 };
 
 export const yelpBusinessSearchAPI = async (
-  cond: conditions,
+    cond: conditions,
 ) => {
-  try {
-    const params: string[] = [
-      `latitude=${cond.latitude}`,
-      `longitude=${cond.longitude}`,
-      `radius=${cond.range}`,
-      `limit=500`,
-    ];
-    const res = await fetch(`${yelpBusinessSearchAPIURL}?${params.join("&")}`, {
-      mode: "cors",
-    });
-    let results = await res.json();
-    return results;
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const params: string[] = [
+            `latitude=${cond.latitude}`,
+            `longitude=${cond.longitude}`,
+            `radius=${cond.range}`,
+            `limit=${cond.limit}`,
+        ];
+        const res = await fetch(`${yelpBusinessSearchAPIURL}?${params.join("&")}`, {
+            mode: "cors",
+        });
+        let results = await res.json();
+        return results;
+    } catch (error) {
+        console.error(error);
+    }
 };
